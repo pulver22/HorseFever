6b3a52cb51fc81324ac5c1118d7b88bda93824c4
niccolo.zamperetti <niccolo.zamperetti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-14 11:13:51 +0000

    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@20 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- provaFinaleIS/src/main/java/provaFinaleIS/Test.java
+++ provaFinaleIS/src/main/java/provaFinaleIS/Test.java
@@ -11,18 +11,6 @@ public class Test {
 		
 		ArrayList<Azione> carteprova=new ArrayList<Azione>(2);
 		
-		Giocatore prova=new Giocatore();
-		Mazzo MazzoAzione=new Mazzo("MazzoAzione");
-		MazzoAzione.mischia();
-		carteprova.add((Azione) MazzoAzione.pesca());
-		carteprova.add((Azione) MazzoAzione.pesca());
-		prova.setCarteAzione(carteprova);
-		
-		prova.setPV(100);
-        prova.setDenari(5000000);
-        prova.scommetti(1);
-        prova.trucca();
-        prova.trucca();
 	}
 
 }
