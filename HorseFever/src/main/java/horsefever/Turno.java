dd8706a47be62a82f8304807a48dd79df455ba62
alessiorossotti <alessiorossotti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-14 13:48:51 +0000

    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@33 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- HorseFever/src/main/java/horsefever/Turno.java
+++ HorseFever/src/main/java/horsefever/Turno.java
@@ -1,8 +1,25 @@
 package horsefever;
+import java.util.ArrayList;
 
 public class Turno {
 	
 	private Partita partita;
+	private ArrayList<Azione> cartedaassegnare;
+	public Turno(Partita par){
+		
+		this.partita=par;
+	}
 	
-
+	public void FaseDistribuzioneCarte(){
+		
+		for(int i=0; i<partita.getNumgiocatori();i++){
+			
+			cartedaassegnare.add((Azione) partita.getMazzoAzione().pesca());
+			cartedaassegnare.add((Azione) partita.getMazzoAzione().pesca());
+			partita.getGiocatori(i).setCarteAzione(cartedaassegnare);
+			
+		}
+		
+		
+	}
 }
