2ba835609feb31da487b32f48d1d50a70ad3a18f
niccolo.zamperetti <niccolo.zamperetti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-14 10:51:01 +0000

    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@17 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- provaFinaleIS/src/main/java/provaFinaleIS/Partita.java
+++ provaFinaleIS/src/main/java/provaFinaleIS/Partita.java
@@ -6,6 +6,7 @@ public class Partita {
 
 	private ArrayList<Giocatore> giocatori;
 	private int numturni=2;
+	private int numgiocatori;
 	private Mazzo mazzoAzione=new Mazzo("MazzoAzione");
 	private Mazzo mazzoPersonaggio=new Mazzo("MazzoPersonaggio");;
 	private Mazzo mazzoMovimento;
@@ -15,11 +16,18 @@ public class Partita {
 	private Turno turno;
 	
 	public Partita(int numgiocatori){
+		this.numgiocatori=numgiocatori;
 		if(numgiocatori==4) numturni=4;
 		else if (numgiocatori==5) numturni=5;
 		else numturni=6;
 		giocatori=new ArrayList<Giocatore>(numgiocatori);
 		
+	}
+	
+	public void preparazione(){
+		for (int i=0; i<numgiocatori; i++){
+			
+		}
 	}
 	
 }
