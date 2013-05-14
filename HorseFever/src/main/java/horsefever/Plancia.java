d16c9dd2fec71a51b30dea19f181ed7257198512
riccardo.polvara <riccardo.polvara@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-14 13:54:56 +0000

    Prototipo di Plancia
    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@35 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- HorseFever/src/main/java/horsefever/Plancia.java
+++ HorseFever/src/main/java/horsefever/Plancia.java
@@ -1,6 +1,21 @@
 package horsefever;
 
+import java.util.ArrayList;
+
 public class Plancia {
+	
+	char[][] corsie = new char[6][30];
+	ArrayList[] corsieTruccate = new ArrayList[6];
+	
+	public Plancia(){
+		for (int i=0;i<=6; i++){
+			corsieTruccate[i]=new ArrayList<Azione>();
+		}
+	}
+	
+	public void TruccaCorsia(Azione carta,int numCorsia){
+		corsieTruccate[numCorsia].add(carta);
+	}
 
 
 }
