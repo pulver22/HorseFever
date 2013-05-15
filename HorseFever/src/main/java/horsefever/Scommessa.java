9a26aa5e5e1196d5e71995c122c2d656c433790f
riccardo.polvara <riccardo.polvara@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-15 14:15:44 +0000

    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@79 09ad4b13-ca6a-4d41-b54e-74912a6b3800

---
+++ HorseFever/src/main/java/horsefever/Scommessa.java
@@ -0,0 +1,16 @@
+package horsefever;
+
+public class Scommessa {
+
+	private int giocatore;
+	private int corsia;
+	private double quota;
+	private char tipoScomessa;
+	
+	public Scommessa(int giocatore,int corsia,double quota,char tipoScommessa){
+		this.giocatore = giocatore;
+		this.corsia = corsia;
+		this.quota = quota;
+		this.tipoScomessa = tipoScommessa;
+	}
+}
