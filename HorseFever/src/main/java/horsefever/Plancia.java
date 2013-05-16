e1aeb116e409ae0196d0cafd336be81ca4f538ee
niccolo.zamperetti <niccolo.zamperetti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-16 14:37:27 +0000

    In Plancia, aggiunte le variabili e i metodi relativi alla possibile implementazione con i Cavalli. I metodi scritti da me sono contraddistinti da quelli di Ricky nel Javadoc mediante la @author Niccolo
    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@122 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- HorseFever/src/main/java/horsefever/Plancia.java
+++ HorseFever/src/main/java/horsefever/Plancia.java
@@ -8,12 +8,16 @@ public class Plancia {
 	boolean[] arrivati = new boolean[6];
 	ArrayList<String> ordineArrivo = new ArrayList<String>(6);
 	int[] posizione = new int[6];
+	private String colori[] = {"Nero","Blu","Verde","Rosso","Giallo","Bianco"}; 
+	ArrayList<Cavallo> cavalli = new ArrayList<Cavallo>(6);
 	ArrayList[] corsieTruccate = new ArrayList[6];
+	private boolean partenza=true;
 	
 	public Plancia(){
 		for (int i=0;i<6; i++){
 			corsieTruccate[i]=new ArrayList<Azione>();		
 			posizione[i]=0;
+			cavalli.add(new Cavallo(colori[i]));
 		}
 	}
 	
@@ -27,6 +31,113 @@ public class Plancia {
 		corsieTruccate[numCorsia-1].add(carta);
 	}
 
+	/**
+	 * @author Niccolo
+	 * Applica gli effetti di tutte le carte Azioni su tutti i Cavalli
+	 * */
+	public void applicaAzioni(){
+		for (int i=0; i<6;i++){
+			
+			eliminaEffettiOpposti(corsieTruccate[i]);
+			assegnaEffettiAlCavallo(corsieTruccate[i],cavalli.get(i));
+		}
+	}
+	
+	/**
+	 * @author Niccolo
+	 * Assegna agli attributi di Cavallo i valori delle Carte Azioni presenti su di esso
+	 * @param ArrayList delle Carte Azione assegnate al cavallo
+	 * @param Il cavallo su cui assegnare gli effetti
+	 * */
+	public void assegnaEffettiAlCavallo(ArrayList azioni, Cavallo cavallo){
+		for (int i=0; i<azioni.size();i++){
+			Azione a= (Azione)azioni.get(i);
+			if (a.getTipoEffetto()=="Partenza") cavallo.setEffettoPartenza(a.getValoreEffetto());
+			if (a.getTipoEffetto()=="Sprint") cavallo.setEffettoSprint(a.getValoreEffetto());
+			if (a.getTipoEffetto()=="Fotofinish") cavallo.setEffettoFotofinish(a.getValoreEffetto());
+			if (a.getTipoEffetto()=="Traguardo") cavallo.setEffettoTraguardo(a.getValoreEffetto());
+			if (a.getTipoEffetto()=="Ultimo" || a.getTipoEffetto()=="Primo") cavallo.setEffettoUltimoPrimo(a.getValoreEffetto());
+		}
+	}
+	
+	/**
+	 * @author Niccolo
+	 * Verifica se in un ArrayList di carte Azione, ce ne sono alcune che si annullano
+	 * @param l'arrayList delle carte Azione da controllare
+	 * */
+	public void eliminaEffettiOpposti(ArrayList azioni){
+		for (int i=0; i<azioni.size()-1; i++){
+			for (int j=1; j<azioni.size(); j++){
+				Azione a1= (Azione)azioni.get(i);
+				Azione a2= (Azione)azioni.get(j);
+				if (a1.getTipoEffetto()==a1.getTipoEffetto()){
+					if (a1.getValoreEffetto().charAt(0)==a2.getValoreEffetto().charAt(0)){
+						azioni.remove(i);
+						azioni.remove(j);
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @author Niccolo
+	 * Esegue i movimenti dei cavalli alla partenza
+	 * @param l'array dei movimenti teorici dei cavalli
+	 * */
+	public void partenza(int[] movimenti){
+		
+		Cavallo c;
+		
+		for (int i=0; i<6;i++){
+			c=cavalli.get(i);
+			c.aggiornaPosizionePartenza(movimenti[i]);
+		}
+	}
+	
+	/**
+	 * @author Niccolo
+	 * Esegue i movimenti dei cavalli successivi alla partenza
+	 * @param l'array dei movimenti teorici dei cavalli
+	 * */
+	public void muovi(int[] movimenti){
+		/*VA ANCORA CONSIDERATO IL FOTOFINISH, E EFFETTI CARTE AZIONE "GRIGIE"*/
+		Cavallo c;
+		int[] dadiSprint=sprint();
+		
+		for(int i=0; i<6; i++){
+			c=cavalli.get(i);
+			c.aggiornaPosizione(movimenti[i]);
+			if (dadiSprint[i]==1) c.aggiornaPosizioneSprint();
+		}
+		
+		
+	}
+	
+	/**
+	 * @author Niccolo
+	 * Esegue lo pseudo lancio dei dadi sprint
+	 * @return un array di 6 elementi. Ciascuno a 0 se non è uscito con nessun lancio, 1 se è uscito 
+	 * */
+	public int[] sprint(){
+		int c;
+		int movimentiSprint[]=new int[6];
+		for (int i=0; i<6; i++){
+			movimentiSprint[i]=0;
+		}
+		for (int j=0; j<2; j++){
+			
+			c=(int)(Math.random()*6);
+			if(movimentiSprint[c]!=1) movimentiSprint[c]=1;
+		
+		}
+		return movimentiSprint;
+		
+	}
+	
+	/**
+	 * Posizione
+	 * */
 	
 	/**
 	 * Se il cavallo non è ancora arrivato al traguardo, ne incrementa la posizione, 
@@ -37,6 +148,7 @@ public class Plancia {
 	 * @param movimento
 	 */
 	public void AggiornaPosizione(int[] movimento){
+		
 		for(int i=0; i<6;i++){
 			if(posizione[i] > 13) ;
 			else posizione[i] += movimento[i];
