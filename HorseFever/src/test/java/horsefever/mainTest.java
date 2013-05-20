db722d733d0a4d0592f6fbbe5cef0f2560fd1c0c
alessiorossotti <alessiorossotti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-20 11:15:52 +0000

    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@148 09ad4b13-ca6a-4d41-b54e-74912a6b3800

---
+++ HorseFever/src/test/java/horsefever/mainTest.java
@@ -0,0 +1,14 @@
+package horsefever;
+
+public class mainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TestPagamento prova=new TestPagamento();
+		prova.testDenariGiocatori();
+
+	}
+
+}
