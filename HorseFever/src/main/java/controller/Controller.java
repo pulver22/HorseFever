eff5af12c412e911fbeb3b03b6720f243b100aa6
niccolo.zamperetti <niccolo.zamperetti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-22 06:22:19 +0000

    Creato semplicementre il package controller con all'interno la classe Controller, nella quale ho copincollato il codice di turno e importato i package View e horsefever
    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@208 09ad4b13-ca6a-4d41-b54e-74912a6b3800

---
+++ HorseFever/src/main/java/controller/Controller.java
@@ -0,0 +1,327 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import horsefever.*;
+import View.*;
+
+public class Controller {
+	private Partita partita;
+	private ArrayList<Azione> carteDaAssegnare;
+	private int[]  posizioniAggiornate=new int[6];
+	private View vista;
+	
+	
+	/**
+	 * Il costruttore di Turno riceve in ingresso Partita per poter utilizzare tutti i dati correnti della partita
+	 *
+	 */
+	public Controller(Partita par){
+		
+		this.partita=par;
+	}
+	
+	public void start(){
+		
+		this.FaseDistribuzioneCarte();
+		this.FaseScommesse();
+		this.FaseCorsa();
+		this.FaseFineTurno();
+		
+	}
+	
+	/**
+	 * Per ciascun giocatore pesca due carte azione dal mazzo delle carte azione 
+	 * e le assegna alle carte azione a disposizione del giocatore
+	 */
+	public void FaseDistribuzioneCarte(){
+		
+		for(int i=0; i<partita.getNumgiocatori();i++){
+			
+			carteDaAssegnare.add((Azione) partita.getMazzoAzione().pesca());
+			carteDaAssegnare.add((Azione) partita.getMazzoAzione().pesca());
+			partita.getGiocatori(i).setCarteAzione(carteDaAssegnare);
+			
+		}	
+	}
+	
+	/**
+	 * Ciascuno dei seguenti metodi:
+	 * - Scommetti con parametro prima scommessa (obbligatoria)
+	 * - Trucca 
+	 * - Scommetti con parametro seconda scommessa (facoltativa)
+	 * Viene chiamato per ciascun giocatore seguendo per i primi due un giro orario partendo dal primo giocatore
+	 * e per il terzo un giro antiorario
+	 */
+	public void FaseScommesse(){
+		
+		Scommessa scom;
+		Giocatore giocatoreCorrente;
+		int[] numSegnalini=partita.getNumSegnalini();
+		int numcorsia;
+		
+		for(int i=0; i<partita.getNumgiocatori();i++){
+			
+			giocatoreCorrente=partita.getGiocatori(i);
+			scom=Scommetti(giocatoreCorrente,1,numSegnalini);
+			numcorsia=scom.getCorsia();
+			numSegnalini[numcorsia]--;
+			partita.getBetManager().AggiungiScommessa(scom);
+		}
+		numSegnalini=partita.getNumSegnalini();
+        
+		for(int i=partita.getNumgiocatori()-1; i>=0;i--){
+			
+        	giocatoreCorrente=partita.getGiocatori(i);
+			scom=Scommetti(giocatoreCorrente,2,numSegnalini);
+        	numcorsia=scom.getCorsia();
+			numSegnalini[numcorsia]--;
+			partita.getBetManager().AggiungiScommessa(scom);
+		}
+        
+        
+		for(int i=0; i<partita.getNumgiocatori();i++){
+			
+			giocatoreCorrente=partita.getGiocatori(i);
+			Trucca(giocatoreCorrente);
+		}
+        
+	}
+	
+	/**
+	 * Fino a che tutti i cavalli non hanno raggiunto il traguardo pesca una carta movimento dal mazzo movimento
+	 * Calcola l'incremento di posizione che deve subire ciascun cavallo in base alla carta movimento
+	 * Quando tutti i cavalli hanno raggiunto il traguardo invoca il BetManager che si occupa del pagamento delle scommesse
+	 * Successivamente chiede alla Lavagna di aggiornare le quotazioni in base all'ordine di arrivo
+	 */
+	public void FaseCorsa(){
+		
+		while(partita.getPlancia().tuttiArrivati()==false){
+			
+			Movimento cartamov=(Movimento) partita.getMazzoMovimento().pesca();
+			
+			posizioniCartaMov(cartamov);
+			
+			
+		}
+		System.out.println(Arrays.toString(partita.getPlancia().getColoriArrivi()));
+		String[] ordineArrivo = partita.getPlancia().getColoriArrivi();
+		String[][] quotazioni= partita.getLavagna().getQuotazioni();
+		
+		partita.getBetManager().Pagamenti(ordineArrivo,quotazioni,partita.getGiocatori());
+		partita.getLavagna().ricalcolaQuotazioni(ordineArrivo);
+		
+	}
+	
+	/**
+	 * I mazzi azione e movimento vengono resettati e mischiati
+	 * Il numero di turni della partita viene incrementato
+	 * Se il turno corrente era l'ultimo turno viene chiamata la fine della partita
+	 */
+	public void FaseFineTurno(){
+		
+		int numeroturni=partita.getNumturni();
+		numeroturni++;
+		
+		partita.setMazzoAzione(new Mazzo("MazzoAzione"));
+		partita.getMazzoAzione().mischia();
+		partita.setMazzoMovimento(new Mazzo("MazzoMovimento"));
+		partita.getMazzoMovimento().mischia();
+		
+		partita.setNumturni(numeroturni);
+	}
+	
+	
+    /**
+     * Chiede a lavagna i valori correnti delle quotazioni dei cavalli e, in base alla carta movimento pescata
+     * costruisce l'array che indica di quanto deve avanzare ciascun cavallo senza effetti delle carte azione
+     * Questo array viene poi passato alla plancia che aggiorna i valori delle posizioni effettive dei cavalli
+     * @param carta movimento
+     */
+    public void posizioniCartaMov(Movimento movimento){
+    	int j,i;
+    	
+    	for(i=0;i<6;i++){ 
+    		posizioniAggiornate[i]=0;
+    	}
+    	
+    	for(i=0;i<6;i++){
+    	
+        j=partita.getLavagna().getRigaMovimento(i);
+        posizioniAggiornate[i]=movimento.getMovimento(j);
+    	
+    	}
+    	System.out.println("Posizioni Aggiornate:");
+    	System.out.println(Arrays.toString(posizioniAggiornate));
+    	partita.getPlancia().muovi(posizioniAggiornate);
+    	System.out.println("Posizioni Cavalli:");
+    	System.out.println(Arrays.toString(partita.getPlancia().getPosizioniCavalli()));
+    }
+    
+    /**
+	 *  @param numscommessa 1:obbligatoria 2:facoltativa
+	 *  Viene verificato se la scommessa è obbligatoria o meno, in tal caso viene chiesto al giocatore
+	 *  se vuole scommettere oppure no, in caso di risposta affermativa viene chiesto al giocatore 
+	 *  il numero di corsia, l'importo e il tipo di scommessa che vuole effettare
+	 *  @return scommessa 
+	 */
+    public Scommessa Scommetti(Giocatore giocatore,int numScommessa,int[] numSegnalini){
+       
+    	
+    	int PV, numCorsia=0;
+    	long denari,importo,scommessaMinima;
+    	char tipoScommessa='N';
+    	boolean buonfine=false;
+    	String[] parametriScommessa=new String[3];
+    	Scommessa scommessa;
+    	
+    	importo=0;
+    	PV=giocatore.getPV();
+    	denari=giocatore.getDenari();
+    	scommessaMinima=PV*100;
+    	
+    	/* se la scommessa non è obbligatoria chiama seconda scommessa che chiede prima all'utente 
+    	 * se vuole ancora scommettere, se i denari sono insufficienti per la scommessa minima 
+    	 * in ogni caso il giocatore non scommette
+    	 */
+    	if(numScommessa==2){
+    		
+    		while(buonfine==false){
+    			
+    			parametriScommessa=vista.chiediSecondaScommessa();
+    			buonfine=true;
+    			
+    			if(denari<scommessaMinima || parametriScommessa[2]=="N"){
+    	
+    				scommessa=new Scommessa(giocatore,10,0,'N');
+    				return scommessa;
+    		
+    			}
+    		
+    			importo=Long.parseLong(parametriScommessa[0]);
+    			numCorsia=Integer.parseInt(parametriScommessa[1]);
+    			tipoScommessa=parametriScommessa[2].charAt(0);
+    		
+    			if(importo>denari || importo<scommessaMinima || numCorsia>7 || numCorsia<0 || (tipoScommessa!='P' && tipoScommessa!='V'))
+    				buonfine=false;
+    		}
+    		scommessa=new Scommessa(giocatore,numCorsia,importo,tipoScommessa);
+    		return scommessa;
+      	
+    	}
+    	/*  Se la scommessa è obbligatoria e il giocatore non ha abbastanza denari
+ 	   gli vengono sottratti 2 PV, se non ha 2 PV il giocatore perde la partita */
+    	if(denari<scommessaMinima){
+    		
+            if(PV<2){   
+            	
+            	System.out.println("Hai perso la partita");
+            	partita.rimuoviGiocatore(giocatore);
+            	scommessa=new Scommessa(giocatore,10,0,'N');
+        		return scommessa;
+            }   
+            else{
+    		       PV=PV-2;
+    		       giocatore.setPV(PV);
+    		       System.out.println("Non hai abbastanza denari per scommettere! Perdi due PV!");
+    		       scommessa=new Scommessa(giocatore,10,0,'N');
+           		   return scommessa;
+            }
+    	}
+    	else{
+    		
+    		while(buonfine==false){
+    			
+    			parametriScommessa=vista.chiediScommessa();
+    			buonfine=true;
+    			
+    			importo=Long.parseLong(parametriScommessa[0]);
+    			numCorsia=Integer.parseInt(parametriScommessa[1]);
+    			tipoScommessa=parametriScommessa[2].charAt(0);
+    	    
+    			if(importo>denari || importo<scommessaMinima || numCorsia>7 || numCorsia<0 || (tipoScommessa!='P' && tipoScommessa!='V'))
+    				buonfine=false;
+        	}
+    		scommessa=new Scommessa(giocatore,numCorsia,importo,tipoScommessa);
+    		return scommessa;
+    	}
+    }
+    
+    /**
+     * Viene chiesto al giocatore quale delle sue carte azione vuole giocare e su quale corsia
+     * applicarla, queste informazioni vengono poi passate al metodo TruccaCorsia in plancia
+     * La carta giocata viene infine rimossa dalle carte a disposizione del giocatore
+     */
+    public void Trucca(Giocatore giocatore){
+        
+    	ArrayList<Azione> carteAzione=new ArrayList<Azione>(2);
+    	carteAzione=giocatore.getCarteAzione();
+    	boolean buonfine = false;
+    	int numcarta=0,numcorsia=0;
+    	System.out.println("Hai in mano queste carte: " );
+    	
+    	for (int i=0;i< carteAzione.size();i++){
+    		System.out.println("" +(i+1)+") " +carteAzione.get(i).getNome()+"," +carteAzione.get(i).getTipoEffetto() +"," +carteAzione.get(i).getValoreEffetto());
+    		}
+    	
+    	while( buonfine==false){	
+    		System.out.println("Scegli quale carta vuoi giocare:");
+        	try{
+        		buonfine=true;
+        	    InputStreamReader reader=new InputStreamReader(System.in);
+        	    BufferedReader myInput=new BufferedReader(reader);
+        	    
+        	    numcarta = Integer.parseInt(""+myInput.readLine());
+        	    
+        	    if(numcarta<1 || numcarta>carteAzione.size()){
+        	    	System.out.println("Errore!! Inserisci un numero da 1 a " +carteAzione.size());
+        	    	buonfine=false;
+        	    }
+        	}
+        	catch(IOException e){
+        		
+        		System.out.println("Errore !!!\n");
+        		buonfine=false;
+        	}
+            catch(NumberFormatException e){
+        		
+        		System.out.println("Errore, ci vuole un numero !!!");
+        		buonfine=false;
+        	}
+    	}
+      	
+    	while(buonfine==false){	
+    	
+    		System.out.println("Inserisci il numero di corsia (1-6) su cui vuoi giocare la carta: ");
+    		try{
+    			buonfine=true;
+    			InputStreamReader reader=new InputStreamReader(System.in);
+    			BufferedReader myInput=new BufferedReader(reader);
+    			numcorsia=Integer.parseInt(""+myInput.readLine());
+    			if(numcorsia<1 || numcorsia>6){
+    	    	
+    				System.out.println("Errore!! Inserisci un numero da 1 a 6");
+    				buonfine=false;
+    			}
+    		}
+    		catch(IOException e){
+    		
+    			System.out.println("Errore !!!\n");
+    			buonfine=false;
+    		}
+    		catch(NumberFormatException e){
+    		
+    			System.out.println("Errore, ci vuole un numero !!!");
+    			buonfine=false;
+    		}
+    	
+    	}
+    	partita.getPlancia().TruccaCorsia(carteAzione.get(numcarta-1), numcorsia);
+    	carteAzione.remove(numcarta -1);
+        giocatore.setCarteAzione(carteAzione);	
+    }
+}
