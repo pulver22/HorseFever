2f6c76412fc5bde2f324a6e3c443ac4a50f3924d
riccardo.polvara <riccardo.polvara@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-24 12:41:39 +0000

    Aggiunta finestra a comparsa per la scommessa
    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@244 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- HorseFever/src/main/java/View/GUIView.java
+++ HorseFever/src/main/java/View/GUIView.java
@@ -1,6 +1,12 @@
 package View;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import javax.swing.*;
 
 import horsefever.Azione;
 import horsefever.Partita;
@@ -11,9 +17,81 @@ public class GUIView implements View{
 
 	@Override
 	public String[] chiediScommessa() {
-		// TODO Auto-generated method stub
-		return null;
+		final JFrame frame = new JFrame("Make your Choice!!");
+		final String[] scommessa = new String[3];
+		
+		JButton invioScommessa = new JButton("Conferma");
+
+		JLabel importo = new JLabel("Inserisci Importo");
+		JTextField sceltaImporto = new JTextField();
+		scommessa[0] = sceltaImporto.getText();
+		
+		
+		JLabel corsia = new JLabel("Scegli la corsia : ");
+		JComboBox corsieDisponibili = new JComboBox();
+		corsieDisponibili.addItem("Corsia N.ro 1");
+		corsieDisponibili.addItem("Corsia N.ro 2");
+		corsieDisponibili.addItem("Corsia N.ro 3");
+		corsieDisponibili.addItem("Corsia N.ro 4");
+		corsieDisponibili.addItem("Corsia N.ro 5");
+		corsieDisponibili.addItem("Corsia N.ro 6");
+		corsieDisponibili.setEditable(true);
+				
+		scommessa[1] =  (String) corsieDisponibili.getSelectedItem();
+		
+		JLabel tipo = new JLabel("Scegli se piazzato o vincente : ");
+		JComboBox tipoScommessa = new JComboBox();
+		tipoScommessa.addItem("Piazzato");
+		tipoScommessa.addItem("Vincente");
+		tipoScommessa.setEditable(true);
+				
+		scommessa[2] =  (String) tipoScommessa.getSelectedItem();
+		
+		
+		invioScommessa.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent ae){
+				//visualizzare messaggio conferma invio e chiudere tutto
+				JOptionPane.showMessageDialog(null,"Hai scommesso " +scommessa[0] +" denari, su  " +scommessa[1] +", come " +scommessa[2]);
+				//vista.scommetti(scommessa);
+				frame.dispose();
+			}
+		});
+		
+		
+		
+			
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(3,2));
+		
+		
+		JPanel panel_conferma = new JPanel();
+		panel_conferma.setLayout(new FlowLayout());
+		
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE) ;
+		frame.add(panel, BorderLayout.NORTH);
+		frame.add(panel_conferma, BorderLayout.SOUTH);
+		
+		panel.add(importo);
+		panel.add(sceltaImporto);
+		panel.add(corsia);
+		panel.add(corsieDisponibili);
+		panel.add(tipo);
+		panel.add(tipoScommessa);
+		
+		panel_conferma.add(invioScommessa);
+		
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(false);
+		
+		return scommessa;
+			
 	}
+		
+	
 
 	@Override
 	public String[] chiediSecondaScommessa() {
--- HorseFever/src/main/java/View/ScommessaGUI.java
+++ HorseFever/src/main/java/View/ScommessaGUI.java
@@ -1,51 +1,86 @@
 package View;
 
 import javax.swing.*;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.*;
 
-public class ScommessaGUI{
+public class ScommessaGUI {
   
 	public static void main(String[] args){
-		final Object[] possibilità = {"Vincente", "Piazzato"};
-		JFrame frame = new JFrame("Make your Choice!!");
-		JButton importo = new JButton("Scegli l'importo");
-		JButton numCorsia = new JButton("Scegli la corsia");
-		JButton tipoScommessa = new JButton("Scegli se piazzato o vincente");
-		importo.addActionListener(new ActionListener(){
+		
+
+		final JFrame frame = new JFrame("Make your Choice!!");
+		final String[] scommessa = new String[3];
+		
+		JButton invioScommessa = new JButton("Conferma");
+
+		JLabel importo = new JLabel("Inserisci Importo");
+		JTextField sceltaImporto = new JTextField();
+		scommessa[0] = sceltaImporto.getText();
+		
+		
+		JLabel corsia = new JLabel("Scegli la corsia : ");
+		JComboBox corsieDisponibili = new JComboBox();
+		corsieDisponibili.addItem("Corsia N.ro 1");
+		corsieDisponibili.addItem("Corsia N.ro 2");
+		corsieDisponibili.addItem("Corsia N.ro 3");
+		corsieDisponibili.addItem("Corsia N.ro 4");
+		corsieDisponibili.addItem("Corsia N.ro 5");
+		corsieDisponibili.addItem("Corsia N.ro 6");
+		corsieDisponibili.setEditable(true);
+				
+		scommessa[1] =  (String) corsieDisponibili.getSelectedItem();
+		
+		JLabel tipo = new JLabel("Scegli se piazzato o vincente : ");
+		JComboBox tipoScommessa = new JComboBox();
+		tipoScommessa.addItem("Piazzato");
+		tipoScommessa.addItem("Vincente");
+		tipoScommessa.setEditable(true);
+				
+		scommessa[2] =  (String) tipoScommessa.getSelectedItem();
+		
+		
+		invioScommessa.addActionListener(new ActionListener(){
+			
 			public void actionPerformed(ActionEvent ae){
-				String str = JOptionPane.showInputDialog(null, "Inserisci l'importo : ", "Scelta Importo", 1);
-				if(str != null)
-					JOptionPane.showMessageDialog(null, "Hai scommesso : " + str,"Scelta Importo", 1);
-				else
-					JOptionPane.showMessageDialog(null, "Hai annullato la scelta.", "Scelta importo", 1);
+				//visualizzare messaggio conferma invio e chiudere tutto
+				JOptionPane.showMessageDialog(null,"Hai scommesso " +scommessa[0] +" denari, su  " +scommessa[1] +", come " +scommessa[2]);
+				//vista.scommetti(scommessa);
+				frame.dispose();
 			}
 		});
 		
-		numCorsia.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent ae){
-				String str = JOptionPane.showInputDialog(null, "Scegli la corsia : ", "Scelta Corsia", 1);
-				if(str != null)
-					JOptionPane.showMessageDialog(null, "Hai scommesso sulla corsia : " + str,"Scelta Corsia", 1);
-				else
-					JOptionPane.showMessageDialog(null, "Hai annullato la scelta.", "Scelta Corsia", 1);
-			}
-		});
 		
-		tipoScommessa.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent ae){
-				String str = (String) JOptionPane.showInputDialog(null, "Scegli se piazzato o vincente : ", "Scelta tipo di Scommessa", JOptionPane.PLAIN_MESSAGE,null, possibilità,null);
-				if(str != null)
-					JOptionPane.showMessageDialog(null, "Hai deciso di scommettere come: " + str,"Scelta tipo di Scommessa", 1);
-				else
-					JOptionPane.showMessageDialog(null, "Hai annullato la scelta.", "Scelta tipo di Scommessa", 1);
-			}
-		});
+		
+			
+		
 		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(3,2));
+		
+		
+		JPanel panel_conferma = new JPanel();
+		panel_conferma.setLayout(new FlowLayout());
+		
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE) ;
+		frame.add(panel, BorderLayout.NORTH);
+		frame.add(panel_conferma, BorderLayout.SOUTH);
+		
 		panel.add(importo);
-		panel.add(numCorsia);
-		frame.add(tipoScommessa);
-		frame.setSize(400, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.add(sceltaImporto);
+		panel.add(corsia);
+		panel.add(corsieDisponibili);
+		panel.add(tipo);
+		panel.add(tipoScommessa);
+		
+		panel_conferma.add(invioScommessa);
+		
+		frame.setLocationRelativeTo(null);
+		frame.pack();
 		frame.setVisible(true);
-	}
+		frame.setResizable(false);
+			}
 	}
