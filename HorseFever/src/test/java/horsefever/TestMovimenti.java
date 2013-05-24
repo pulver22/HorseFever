72ee19258a53bb47a3801040c5638afa05074fce
niccolo.zamperetti <niccolo.zamperetti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-24 08:10:44 +0000

    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@242 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- HorseFever/src/test/java/horsefever/TestMovimenti.java
+++ HorseFever/src/test/java/horsefever/TestMovimenti.java
@@ -1,5 +1,10 @@
 package horsefever;
 
+import View.*;
+import adapter.*;
+import controller.*;
+import horsefever.*;
+
 /**
  * testa i movimenti senza gli effetti delle carte Azione
  * @author alessiorossotti
@@ -7,13 +12,25 @@ package horsefever;
  */
 public class TestMovimenti {
 
-	 private Partita partitaprova;
+	private Partita partitaprova;
+	 private Controller controller;
+	 private Adapter adapter;
+	 private TextView view;
+	 
 	 
   
 	 public TestMovimenti(){
-		 
 		 partitaprova=new Partita(6);
+		 controller=new Controller(partitaprova);
+		 adapter=new Adapter();
+		 view =new TextView();
+		 controller.setView(view);
+		 partitaprova.setAdapter(adapter);
+		 adapter.addView(view);
 		 
-		 
+	 }
+	 
+	 public void startTest(){
+		 controller.FaseCorsa();
 	 }
 }
--- HorseFever/src/test/java/horsefever/mainTest.java
+++ HorseFever/src/test/java/horsefever/mainTest.java
@@ -1,5 +1,9 @@
 package horsefever;
 
+import View.TextView;
+import adapter.Adapter;
+import controller.Controller;
+
 public class mainTest {
 
 	
@@ -11,9 +15,20 @@ public class mainTest {
 		prova.setUp();
 		prova.testDenariGiocatori();
         */
+		Partita p=new Partita(6);
+		Controller c=new Controller(p);
+		Adapter ad = new Adapter();
+		TextView v=new TextView();
+		c.setView(v);
+		ad.addView(v);
+		p.setAdapter(ad);
+		for (int i=0; i<6;i++){
+			Azione a=(Azione)p.getMazzoAzione().pesca();
+			p.getPlancia().TruccaCorsia(a, i+1);
+			System.out.println(a.toString());
+		}
 		
-		TestMovimenti prova=new TestMovimenti();
-		
+		c.FaseCorsa();
 	}
 
 }
