230064de2a8c9d19f8fd818fbf88dccdf363fa31
alessiorossotti <alessiorossotti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-27 13:07:44 +0000

    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@274 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- HorseFever/src/main/java/View/Board.java
+++ HorseFever/src/main/java/View/Board.java
@@ -2,24 +2,27 @@ package View;
 
 import javax.swing.*;
 import javax.swing.border.*;
-
 import java.awt.*;
 
 public class Board extends JFrame {
 
 	private JTextArea areaNotifica;
+	private JTextArea quotazioni=new JTextArea();
     private JScrollPane scroll;
+    private Background background=new Background("sfondo.jpg");
 	private JPanel pannelloNotifica=new JPanel();
 	private JPanel pannelloPlancia=new JPanel();
+	private JPanel carteMovimento=new JPanel();
 	private JPanel pannelloGiocatore=new JPanel();
 	private JPanel pannelloLavagna=new JPanel();
 	private Image plancia;
-	private Image sfondo;
+	private Image movimento;
 	
     public Board(){
     	
     	//toglie il Layout di default del JFrame
-    	getContentPane().setLayout(null);
+    	//getContentPane().setLayout(null);
+    	
     	
     	//Area Notifica
     	
@@ -28,12 +31,12 @@ public class Board extends JFrame {
     	scroll=new JScrollPane(areaNotifica);
     	scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
     
-    	pannelloNotifica.setBounds(50,50,300,250);
+    	pannelloNotifica.setBounds(50,50,350,250);
     	pannelloNotifica.setLayout(new BorderLayout());
     	pannelloNotifica.setBorder ( new TitledBorder ( new BevelBorder(BevelBorder.RAISED), "Area Notifica" ) );
         pannelloNotifica.add(scroll);
     	pannelloNotifica.setVisible(true);
-    	pannelloNotifica.setBackground(Color.ORANGE);
+    	pannelloNotifica.setBackground(Color.decode("#FFFFFF"));
     	
     	//Plancia
     	pannelloPlancia.setBounds(450,50,650,300);
@@ -41,33 +44,37 @@ public class Board extends JFrame {
         pannelloPlancia.setBackground(Color.white);
        
         ImageIcon ii=new ImageIcon(this.getClass().getResource("plancia.jpg"));
-		plancia=ii.getImage();
+		plancia=ii.getImage();	
+		ii=new ImageIcon(this.getClass().getResource("horseFever-103.jpg"));
+		movimento=ii.getImage();
 		
-		ImageIcon ii2=new ImageIcon(this.getClass().getResource("sfondo.jpg"));
-		sfondo=ii2.getImage();
 		
 		//Giocatore
 		
-		pannelloGiocatore.setBounds(450,400,500,250);
-		pannelloGiocatore.setBackground(Color.orange);
+		pannelloGiocatore.setBounds(450,380,650,270);
+		pannelloGiocatore.setBackground(Color.decode("#FFFFFF"));
 		pannelloGiocatore.setBorder (new TitledBorder (new BevelBorder(BevelBorder.RAISED),"Giocatore"));
-		
+		pannelloGiocatore.setVisible(true);
 		
 		//Lavagna
-		pannelloLavagna.setBounds(50,400,300,250);
-		pannelloLavagna.setBackground(Color.orange);
+		pannelloLavagna.setBounds(50,350,350,300);
+		pannelloLavagna.setBackground(Color.decode("#FFFFFF"));
 		pannelloLavagna.setBorder ( new TitledBorder (new BevelBorder(BevelBorder.RAISED), "Lavagna" ) );
+		pannelloLavagna.setVisible(true);
+		carteMovimento.setBackground(Color.black);
+		pannelloLavagna.add(carteMovimento);
+		pannelloLavagna.add(quotazioni);
 		
-		this.add(pannelloGiocatore);
-    	this.add(pannelloNotifica);
+    	
+		this.add(pannelloNotifica);
     	this.add(pannelloLavagna);
     	//this.add(pannelloPlancia);
     	this.add(pannelloGiocatore);
+    	this.add(background);
+    
     	
-    	
-    	
-    	this.setBackground(Color.gray);
     	this.setResizable(false);
+    	this.setTitle("Horse Fever");
     	this.setSize(1200,700);
     	this.setVisible(true);
     	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -84,8 +91,9 @@ public class Board extends JFrame {
         super.paint(g);
         
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(sfondo, 0,0,null);
         g2d.drawImage(plancia, 450,50, null);
+        g2d.drawImage(movimento, 60,360,null);
+        
         
     
 	}
