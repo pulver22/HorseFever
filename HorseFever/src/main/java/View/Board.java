f5e5a0cd5c3ae82a954bbf38f8e4ad81a50ab3b5
alessiorossotti <alessiorossotti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-27 10:08:58 +0000

    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@260 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- HorseFever/src/main/java/View/Board.java
+++ HorseFever/src/main/java/View/Board.java
@@ -11,31 +11,54 @@ public class Board extends JFrame {
     private JScrollPane scroll;
 	private JPanel pannelloNotifica=new JPanel();
 	private JPanel pannelloPlancia=new JPanel();
+	private JPanel pannelloGiocatore=new JPanel();
+	private JPanel pannelloLavagna=new JPanel();
+	private Image plancia;
 	
     public Board(){
     	
     	//toglie il Layout di default del JFrame
     	getContentPane().setLayout(null);
     	
+    	//Area Notifica
+    	
     	areaNotifica=new JTextArea();
-    	//areaNotifica.setEditable(false);
+    	areaNotifica.setEditable(false);
     	scroll=new JScrollPane(areaNotifica);
     	scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
-    	
-    	
+    
     	pannelloNotifica.setBounds(50,50,300,250);
     	pannelloNotifica.setLayout(new BorderLayout());
-    	pannelloNotifica.setBorder ( new TitledBorder ( new EtchedBorder (), "Area Notifica" ) );
+    	pannelloNotifica.setBorder ( new TitledBorder ( new BevelBorder(BevelBorder.RAISED), "Area Notifica" ) );
         pannelloNotifica.add(scroll);
     	pannelloNotifica.setVisible(true);
     	pannelloNotifica.setBackground(Color.ORANGE);
     	
+    	//Plancia
     	pannelloPlancia.setBounds(450,50,650,300);
         pannelloPlancia.setVisible(true);
         pannelloPlancia.setBackground(Color.white);
-        
+       
+        ImageIcon ii=new ImageIcon(this.getClass().getResource("plancia.jpg"));
+		plancia=ii.getImage();
+		
+		//Giocatore
+		
+		pannelloGiocatore.setBounds(450,400,500,250);
+		pannelloGiocatore.setBackground(Color.orange);
+		pannelloGiocatore.setBorder (new TitledBorder (new BevelBorder(BevelBorder.RAISED),"Giocatore"));
+		
+		
+		//Lavagna
+		pannelloLavagna.setBounds(50,400,300,250);
+		pannelloLavagna.setBackground(Color.orange);
+		pannelloLavagna.setBorder ( new TitledBorder (new BevelBorder(BevelBorder.RAISED), "Lavagna" ) );
+		
+		this.add(pannelloGiocatore);
     	this.add(pannelloNotifica);
-    	this.add(pannelloPlancia);
+    	this.add(pannelloLavagna);
+    	//this.add(pannelloPlancia);
+    	this.add(pannelloGiocatore);
     	
     	this.setBackground(Color.gray);
     	this.setResizable(false);
@@ -50,5 +73,14 @@ public class Board extends JFrame {
 		Board prova=new Board();
 
 	}
+	
+	public void paint(Graphics g) {
+        super.paint(g);
+        
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(plancia, 450,50, null);
+    
+	}
+	
 
 }
