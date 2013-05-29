9d10562c377951a4389b92825365f786f02ffc01
niccolo.zamperetti <niccolo.zamperetti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-29 14:47:25 +0000

    Test su Traguardo e PrimoUltimo di Cavallo a buon esito
    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@318 09ad4b13-ca6a-4d41-b54e-74912a6b3800

---
+++ HorseFever/src/test/java/horsefever/TestCavalloPrimoUltimo.java
@@ -0,0 +1,68 @@
+package horsefever;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+public class TestCavalloPrimoUltimo {
+
+	Cavallo cavallo;
+	Azione azione1;
+	Azione azione2;
+	
+	@Before
+	public void setup(){
+		cavallo=new Cavallo("Nero");
+		azione1=new Azione("Vigor Ferreum","Verde","Utimo","=4",'G');
+		azione2=new Azione("Feliz Infernalis","Rosso","Primo","=0",'G');
+	}
+	
+	@Test
+	public void casoTestPrimo1(){
+		int incremento=2;
+		cavallo.setEffettoUltimoPrimo(azione1.getValoreEffetto());
+		cavallo.aggiornaPosizionePrimoUltimo(incremento,true,false);
+		assertEquals(2,cavallo.getPosizione());
+	}
+	
+	@Test
+	public void casoTestPrimo2(){
+		int incremento=2;
+		cavallo.setEffettoUltimoPrimo(azione2.getValoreEffetto());
+		cavallo.aggiornaPosizionePrimoUltimo(incremento,true,false);
+		assertEquals(0,cavallo.getPosizione());
+	}
+	
+	@Test
+	public void casoTestUltimo1(){
+		int incremento=2;
+		cavallo.setEffettoUltimoPrimo(azione1.getValoreEffetto());
+		cavallo.aggiornaPosizionePrimoUltimo(incremento,false,true);
+		assertEquals(4,cavallo.getPosizione());
+	}
+	
+	@Test
+	public void casoTestUltimo2(){
+		int incremento=2;
+		cavallo.setEffettoUltimoPrimo(azione2.getValoreEffetto());
+		cavallo.aggiornaPosizionePrimoUltimo(incremento,false,true);
+		assertEquals(2,cavallo.getPosizione());
+	}
+	
+	@Test
+	public void casoTestNessuno1(){
+		int incremento=2;
+		cavallo.setEffettoUltimoPrimo(azione2.getValoreEffetto());
+		cavallo.aggiornaPosizionePrimoUltimo(incremento,false,false);
+		assertEquals(2,cavallo.getPosizione());
+	}
+	
+	@Test
+	public void casoTestNessuno2(){
+		int incremento=2;
+		cavallo.setEffettoUltimoPrimo(azione2.getValoreEffetto());
+		cavallo.aggiornaPosizionePrimoUltimo(incremento,false,false);
+		assertEquals(2,cavallo.getPosizione());
+	}
+	
+}
---
+++ HorseFever/src/test/java/horsefever/TestCavalloTraguardo.java
@@ -0,0 +1,63 @@
+package horsefever;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+public class TestCavalloTraguardo {
+
+	Cavallo cavallo;
+	Azione azione1;
+	Azione azione2;
+	
+	@Before
+	public void setup(){
+		cavallo=new Cavallo("Nero");
+		azione1=new Azione("Fustis et Radix","Verde","Traguardo","+2",'F');
+		azione2=new Azione("XIII","Rosso","Traguardo","=0",'F');
+	}
+	
+	@Test
+	public void casoTestTraguardo1(){
+		int incremento=2;
+		cavallo.setPosizione(12);
+		cavallo.setEffettoTraguardo(azione1.getValoreEffetto());
+		cavallo.aggiornaPosizione(incremento);
+		assertEquals(16,cavallo.getPosizione());
+	}
+	
+	@Test
+	public void casoTestTraguardo2(){
+		int incremento=2;
+		cavallo.setPosizione(12);
+		cavallo.setEffettoTraguardo(azione2.getValoreEffetto());
+		cavallo.aggiornaPosizione(incremento);
+		assertEquals(13,cavallo.getPosizione());
+	}
+	
+	@Test
+	public void casoTestNoTraguardo1(){
+		int incremento=2;
+		cavallo.setPosizione(10);
+		cavallo.setEffettoTraguardo(azione2.getValoreEffetto());
+		cavallo.aggiornaPosizione(incremento);
+		assertEquals(12,cavallo.getPosizione());
+	}
+	
+	@Test
+	public void casoTestNoTraguardo2(){
+		int incremento=2;
+		cavallo.setPosizione(10);
+		cavallo.setEffettoTraguardo(azione2.getValoreEffetto());
+		cavallo.aggiornaPosizione(incremento);
+		assertEquals(12,cavallo.getPosizione());
+	}
+	
+	@Test
+	public void casoTestNessuno(){
+		int incremento=2;
+		cavallo.aggiornaPosizione(incremento);
+		assertEquals(2,cavallo.getPosizione());
+	}
+	
+}
