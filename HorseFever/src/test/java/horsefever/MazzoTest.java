8cfb6505b90cf25d50fe244249539ebee6a7fb0a
riccardo.polvara <riccardo.polvara@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-05-29 13:18:50 +0000

    Eseguiti test sul mischi e l'inserimento di una carta in mazzo
    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@314 09ad4b13-ca6a-4d41-b54e-74912a6b3800

---
+++ HorseFever/src/test/java/horsefever/MazzoTest.java
@@ -0,0 +1,34 @@
+package horsefever;
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class MazzoTest {
+	public Mazzo deck = new Mazzo("MazzoPersonaggio");
+	public Mazzo deck2 = new Mazzo("MazzoPersonaggio");
+	
+	@Test
+	public void mischiaTest(){
+		deck.mischia();
+		
+		
+		assertNotSame(deck.pescaCertezza(0),deck2.pescaCertezza(0));
+		assertNotSame(deck.pescaCertezza(1),deck2.pescaCertezza(1));
+		assertNotSame(deck.pescaCertezza(2),deck2.pescaCertezza(2));
+		assertNotSame(deck.pescaCertezza(3),deck2.pescaCertezza(3));
+		assertNotSame(deck.pescaCertezza(4),deck2.pescaCertezza(4));
+		assertNotSame(deck.pescaCertezza(5),deck2.pescaCertezza(5));
+		
+		
+	}
+	
+	@Test 
+	public void inserisciTest(){
+		Personaggio carta = new Personaggio("prova",100,"5");
+		deck.inserisci(carta);
+		assertSame(carta,deck.pescaCertezza(6));
+	}
+	
+	
+
+}
