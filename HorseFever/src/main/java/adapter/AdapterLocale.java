896ec3168db774ca8a90464a67f9ab3a10e41b93
niccolo.zamperetti <niccolo.zamperetti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-06-02 15:01:27 +0000

    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@355 09ad4b13-ca6a-4d41-b54e-74912a6b3800

---
+++ HorseFever/src/main/java/adapter/AdapterLocale.java
@@ -0,0 +1,60 @@
+package adapter;
+
+import horsefever.Azione;
+
+import java.util.ArrayList;
+import View.*;
+import eventi.HorseFeverEvent;
+
+public class AdapterLocale {
+
+	ArrayList<View> viewRegistrate=new ArrayList<View>();
+
+	public String[] chiediScommessa(int indice) {
+		
+		String[] valori=new String[3];
+		
+		valori=viewRegistrate.get(0).chiediScommessa();
+		
+		
+		return valori;
+	}
+	
+	public String[] chiediSecondaScommessa(int indice){
+		
+        String[] valori=new String[3];
+        
+        valori=viewRegistrate.get(0).chiediSecondaScommessa();
+		
+		return valori;
+	}
+	
+	public String[] chiediTrucca(ArrayList<Azione> carteAzione, int indice) {
+		
+		String[] scelta = new String[2];
+		
+		scelta=viewRegistrate.get(0).chiediTrucca(carteAzione);
+		
+		return scelta;
+	}
+	
+	public void stampaMessaggio(String messaggio,int indice){
+		
+		viewRegistrate.get(0).stampaMessaggio(messaggio);
+	}
+
+	public void notify(HorseFeverEvent e){
+		
+		for (View v: viewRegistrate){
+			v.notify(e);
+		}
+	}
+	
+	
+	public void addView(View v){
+		viewRegistrate.add(v);
+	}
+	public void removeView(int i){
+		viewRegistrate.remove(i);
+	}
+}
