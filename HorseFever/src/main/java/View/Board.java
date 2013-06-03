4dcceb6e484d771351e4134311dc464aca6030dd
alessiorossotti <alessiorossotti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-06-03 13:31:02 +0000

    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@368 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- HorseFever/src/main/java/View/Board.java
+++ HorseFever/src/main/java/View/Board.java
@@ -59,7 +59,9 @@ public class Board extends JFrame {
         pannelloTurni.add(numTurno);
         pannelloTurni.setVisible(true);
         pannelloTurni.setBackground(Color.decode("#d6a45f"));
-    
+        numTurno.setEditable(false);
+        
+        
     	//inizializzazione Pedine
     	
     	pedine[0]=new Pedina(455,90,"pedine/nero.png",this);
@@ -168,11 +170,7 @@ public class Board extends JFrame {
     	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
-    public static void main(String args[]){
-    	
-    	Board prova=new Board();
-    }
-	
+   
 	public void paint(Graphics g) {
         super.paint(g);
         
