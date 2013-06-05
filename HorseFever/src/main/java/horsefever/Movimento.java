0e81d1e34e2ee587bab78cf477ca1e35e53fe365
alessiorossotti <alessiorossotti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-06-05 15:47:30 +0000

    security fix
    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@449 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- HorseFever/src/main/java/horsefever/Movimento.java
+++ HorseFever/src/main/java/horsefever/Movimento.java
@@ -1,8 +1,5 @@
 package horsefever;
 
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
 
 public class Movimento extends Carta {
 
@@ -11,20 +8,20 @@ public class Movimento extends Carta {
    
    public Movimento(int[] valori,String nome){
 	   
-	   this.valorimov=valori;
-	   this.nomefile=nome;
+	   this.valorimov=valori.clone();
+	   this.nomefile=String.valueOf(nome);
    }
    
   
 
 public Movimento(int m1,int m2, int m3, int m4, int m5, int m6,String nome){
-	   valorimov[0]=m1;
-	   valorimov[1]=m2;
-	   valorimov[2]=m3;
-	   valorimov[3]=m4;
-	   valorimov[4]=m5;
-	   valorimov[5]=m6;
-	   this.nomefile=nome;
+	   valorimov[0]=Integer.valueOf(m1);
+	   valorimov[1]=Integer.valueOf(m2);
+	   valorimov[2]=Integer.valueOf(m3);
+	   valorimov[3]=Integer.valueOf(m4);
+	   valorimov[4]=Integer.valueOf(m5);
+	   valorimov[5]=Integer.valueOf(m6);
+	   this.nomefile=String.valueOf(nome);
 	   
    }
    
