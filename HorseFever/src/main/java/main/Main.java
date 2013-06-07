07c511d8e808e63b9b1e10080709346c2869bbd8
niccolo.zamperetti <niccolo.zamperetti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-06-07 07:22:01 +0000

    Aggiunti al main le scelte su numeroGiocatori, grafica/testuale, rete/locale (Quest'ultima per ora fa scegliere sempre e solo locale)
    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@474 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- HorseFever/src/main/java/main/Main.java
+++ HorseFever/src/main/java/main/Main.java
@@ -12,6 +12,11 @@ public class Main {
 
 	public static void main(String[] args){
 	
+		int numGiocatori;
+		String sceltaGiocatori;
+		String[] opzioniGiocatori = {"2","3","4","5","6"};
+		sceltaGiocatori = (String)JOptionPane.showInputDialog(null, "Quanti sono i giocatori?","Numero Giocatori", JOptionPane.PLAIN_MESSAGE,null, opzioniGiocatori, opzioniGiocatori);
+		numGiocatori=Integer.parseInt(sceltaGiocatori);
 		View tv;
 		String sceltaVista;
 		String[] opzioniVista = {"Graphic User Interface","Testuale"};
@@ -20,9 +25,16 @@ public class Main {
 			tv= new TextView();
 		else 
 			tv= new GUIView();
-		Adapter ad=new AdapterLocale();
+		String sceltaRete;
+		String[] opzioniRete = {"Locale"};
+		Adapter ad;
+		sceltaRete = (String)JOptionPane.showInputDialog(null, "Vuoi giocare in rete o in locale?","Opzioni Rete", JOptionPane.PLAIN_MESSAGE,null, opzioniRete, opzioniRete);
+		if (sceltaRete.equals("Locale"))
+			ad=new AdapterLocale();
+		else 
+			ad=new AdapterLocale();
 		ad.addView(tv);
-		Partita p=new Partita(2,ad);
+		Partita p=new Partita(numGiocatori,ad);
 		Controller c = new Controller(p);
 		c.setAdapter(ad);
 		c.start();
