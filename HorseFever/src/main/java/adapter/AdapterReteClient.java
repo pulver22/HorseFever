18a94074d80e77c4d3a9ed15e2b9ca28a5091bac
niccolo.zamperetti <niccolo.zamperetti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-06-07 06:52:34 +0000

    Create classi adapter server e client che implementano adapter e adapterClientHandler. Guardando il codice d'esempio della SocketChat di Sangiorgio implementeremo effettivamente i metodi.
    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@471 09ad4b13-ca6a-4d41-b54e-74912a6b3800

---
+++ HorseFever/src/main/java/adapter/AdapterReteClient.java
@@ -0,0 +1,60 @@
+package adapter;
+
+import horsefever.Azione;
+
+import java.util.ArrayList;
+
+import View.View;
+import eventi.HorseFeverEvent;
+
+public class AdapterReteClient implements Adapter{
+
+	@Override
+	public String[] chiediScommessa(int indice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] chiediSecondaScommessa(int indice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] chiediTrucca(ArrayList<Azione> carteAzione, int indice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void stampaMessaggio(String messaggio, int indice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notify(HorseFeverEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void prosegui(String messaggio, int indice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addView(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeView(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- HorseFever/src/main/java/adapter/AdapterReteServer.java
+++ HorseFever/src/main/java/adapter/AdapterReteServer.java
@@ -7,76 +7,52 @@ import java.util.ArrayList;
 import View.View;
 import eventi.HorseFeverEvent;
 
-public class AdapterRete implements Adapter{
-	
-	/**
-	 * Chiede ad ogni giocatore registrato di fare una scommessa
-	 */
+public class AdapterReteServer implements Adapter{
+
 	@Override
 	public String[] chiediScommessa(int indice) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	/**
-	 * Chiedi ad ogni giocatore registrato di effettuare la seconda scommessa
-	 */
 	@Override
 	public String[] chiediSecondaScommessa(int indice) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	/**
-	 * Chiedi ad ogni giocatore registrato di truccare la corsa
-	 */
 	@Override
 	public String[] chiediTrucca(ArrayList<Azione> carteAzione, int indice) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	/**
-	 * Stampa a video di ogni giocatore registrato un messaggio
-	 */
 	@Override
 	public void stampaMessaggio(String messaggio, int indice) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	/**
-	 * Notifica ad ogni giocatore registrato un evento
-	 */
 	@Override
 	public void notify(HorseFeverEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	/**
-	 * Aggiunge una view, quindi registra un giocatore
-	 */
+	@Override
+	public void prosegui(String messaggio, int indice) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	@Override
 	public void addView(View v) {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	/**
-    * Rimuove una view, quindi elimina un giocatore dal gioco
-	 */
+
 	@Override
 	public void removeView(int i) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/**
-	 * Permette di avanzare alla fase successiva del turno, e quindi del gioco
-	 */
-	@Override
-	public void prosegui(String messaggio, int indice) {
 		// TODO Auto-generated method stub
 		
 	}
