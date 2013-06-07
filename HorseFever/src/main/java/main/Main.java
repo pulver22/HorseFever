3cef055ebd85ddec686e219c5563e3e68a5f5251
niccolo.zamperetti <niccolo.zamperetti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-06-07 07:09:56 +0000

    Main del gioco ora fa scegliere la vista da usare per il gioco
    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@472 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- HorseFever/src/main/java/main/Main.java
+++ HorseFever/src/main/java/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import javax.swing.JOptionPane;
+
 import horsefever.*;
 import View.*;
 import controller.*;
@@ -10,8 +12,14 @@ public class Main {
 
 	public static void main(String[] args){
 	
-
-		TextView tv= new TextView();
+		View tv;
+		String sceltaVista;
+		String[] opzioniVista = {"Graphic User Interface","Testuale"};
+		sceltaVista = (String)JOptionPane.showInputDialog(null, "Con quale interfaccia vuoi giocare?","Opzioni Vista", JOptionPane.PLAIN_MESSAGE,null, opzioniVista, opzioniVista);
+		if (sceltaVista.equals("Testuale"))
+			tv= new TextView();
+		else 
+			tv= new GUIView();
 		Adapter ad=new AdapterLocale();
 		ad.addView(tv);
 		Partita p=new Partita(2,ad);
