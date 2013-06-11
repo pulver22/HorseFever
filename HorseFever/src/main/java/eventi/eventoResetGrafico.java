994d22500abe96281c877882d8e391a48a779e29
alessiorossotti <alessiorossotti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-06-11 11:48:57 +0000

    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@505 09ad4b13-ca6a-4d41-b54e-74912a6b3800

---
+++ HorseFever/src/main/java/eventi/eventoResetGrafico.java
@@ -0,0 +1,14 @@
+package eventi;
+
+public class eventoResetGrafico extends HorseFeverEvent {
+
+	
+	@Override
+	public String rappresentazione() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
