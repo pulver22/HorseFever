95f5249e3da6dec34e886e6e3d9a763d15165d80
niccolo.zamperetti <niccolo.zamperetti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-06-14 16:50:19 +0000

    Aggiunto Ulteriore test per il Branch Coverage
    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@588 09ad4b13-ca6a-4d41-b54e-74912a6b3800

---
+++ HorseFever/src/test/java/horsefever/TestCavalloQuotazione.java
@@ -0,0 +1,48 @@
+package horsefever;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCavalloQuotazione {
+
+	Cavallo cavallo;
+	Azione azione1;
+	Azione azione2;
+	
+	@Before
+	public void setup(){
+		cavallo=new Cavallo("Nero");
+		cavallo.setQuotazione(4);
+		azione1=new Azione("Alfio Allibratore","Grigio","Quotazione","+2",'W',14);
+		azione2=new Azione("Steven Sting","Grigio","Quotazione","-2",'Y',16);
+	}
+	
+	@Test
+	public void casoTestQuot1(){
+		cavallo.setEffettoQuotazione(azione1.getValoreEffetto());
+		assertEquals(2,cavallo.getQuotazione());
+	}
+	
+	@Test
+	public void casoTestQuot2(){
+		cavallo.setEffettoQuotazione(azione2.getValoreEffetto());
+		assertEquals(6,cavallo.getQuotazione());
+	}
+	
+	@Test
+	public void casoTestQuotLimite1(){
+		cavallo.setQuotazione(3);
+		cavallo.setEffettoQuotazione(azione1.getValoreEffetto());
+		assertEquals(2,cavallo.getQuotazione());
+	}
+	
+	@Test
+	public void casoTestQuotLimite2(){
+		cavallo.setQuotazione(6);
+		cavallo.setEffettoQuotazione(azione2.getValoreEffetto());
+		assertEquals(7,cavallo.getQuotazione());
+	}
+	
+}
