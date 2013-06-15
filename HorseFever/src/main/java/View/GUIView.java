a574a0a8964c6d6a54045a7892c418db4408edd8
niccolo.zamperetti <niccolo.zamperetti@09ad4b13-ca6a-4d41-b54e-74912a6b3800>
2013-06-15 07:55:28 +0000

    git-svn-id: https://subversion.assembla.com/svn/riccardo-polvara-alessio-rossotti-niccolo-zamperetti@594 09ad4b13-ca6a-4d41-b54e-74912a6b3800

--- HorseFever/src/main/java/View/GUIView.java
+++ HorseFever/src/main/java/View/GUIView.java
@@ -1,17 +1,5 @@
 package View;
 
-/*
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.TextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-*/
 
 import javax.swing.*;
 
